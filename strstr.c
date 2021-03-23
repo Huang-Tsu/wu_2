1f7f484a296269a9dbb1098f31bc892009f7ba1d
Huang-Tsu <76726586+Huang-Tsu@users.noreply.github.com>
2021-03-23 15:55:55 +0800

    Add files via upload

--- strstr.c
+++ strstr.c
@@ -6,10 +6,9 @@ int str_len(char *input){
 	int cnt = 0;
 	while(*input++) cnt++;
 	return cnt;
-
 }
 int str_n_cmp(char *str1, char *str2, int len){
-	for(int i=0; i<len; i++){
+	for(int i=0; i<len-1; i++){
 		if(*str1!='\0' && *str2!='\0' && *str1==*str2){
 			str1 ++;
 			str2 ++;
@@ -25,9 +24,11 @@ int str_n_cmp(char *str1, char *str2, int len){
 char *my_strstr(char *input, char *pattern){
 	int input_len = str_len(input);
 	int pattern_len = str_len(pattern);
-		printf("input_len:%d\tpattern_len:%d\n", input_len, pattern_len);
+		printf("src_ptr in strstr:%p\n", input);
 	for(int i=0; i<=input_len-pattern_len; i++){
+			printf("str_n_cmp(input+i, pattern, pattern_len):%d\n", str_n_cmp(input+i, pattern, pattern_len));
 		if(str_n_cmp(input+i, pattern, pattern_len)==0){
+			printf("input+i:%p\n", input+i);
 			return input+i;
 		}
 	}
@@ -50,13 +51,13 @@ int main(){
 		while( (src_ptr=my_strstr(src_ptr, target)) != NULL){
 				printf("src_ptr:%p\n", src_ptr);
 			final_ptr = src_ptr;
+				printf("final_ptr:%p\n", final_ptr);
+				puts("");
 		}
 			printf("final_ptr:%p\n", final_ptr);	
 		if(final_ptr){
 					puts("HI, in if(final_ptr)");
-			for(char *i=src; i<final_ptr; i++){
-				idx ++;
-			}
+			idx = final_ptr-src;
 			printf("%d,%d\n", idx, idx+target_len);
 		}
 		else{
