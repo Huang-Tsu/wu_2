30e70af0ce871dd3f525a8295be1cf3215f5c4aa
Huang-Tsu <76726586+Huang-Tsu@users.noreply.github.com>
2021-03-25 17:02:58 +0800

    Add files via upload

--- strstr.c
+++ strstr.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-	#include <unistd.h>
 
 int str_len(char *input){
 	int cnt = 0;
@@ -17,18 +16,13 @@ int str_n_cmp(char *str1, char *str2, int len){
 			break;	
 		}
 	}
-		printf("*str1-*str2:%d\n", *str1-*str2);
-		usleep(1000000);	
 	return (*str1 - *str2);
 }
 char *my_strstr(char *input, char *pattern){
 	int input_len = str_len(input);
 	int pattern_len = str_len(pattern);
-		printf("src_ptr in strstr:%p\n", input);
 	for(int i=0; i<=input_len-pattern_len; i++){
-			printf("str_n_cmp(input+i, pattern, pattern_len):%d\n", str_n_cmp(input+i, pattern, pattern_len));
 		if(str_n_cmp(input+i, pattern, pattern_len)==0){
-			printf("input+i:%p\n", input+i);
 			return input+i;
 		}
 	}
@@ -38,30 +32,28 @@ int main(){
 
 	char whole[3073];
 	char src[2049];
-	char *src_ptr = src;
-	char *final_ptr = NULL;
+	char *src_ptr;
+	char *final_ptr;
 	char target[1024]; 
 	int idx = 0;
 	int target_len;
 
 	while(fgets(whole, 3073, stdin) != NULL){	
-		sscanf(whole, "%[^,],%s", src, target);
+
+		sscanf(whole, "%[^,],%[^\n]", src, target);
+
+		src_ptr = src;
+		final_ptr = NULL;
 		target_len = str_len(target);
-				printf("src:%s\ttarget:%s\n", src, target);
 		while( (src_ptr=my_strstr(src_ptr, target)) != NULL){
-				printf("src_ptr:%p\n", src_ptr);
 			final_ptr = src_ptr;
-				printf("final_ptr:%p\n", final_ptr);
-				puts("");
+			src_ptr ++;
 		}
-			printf("final_ptr:%p\n", final_ptr);	
 		if(final_ptr){
-					puts("HI, in if(final_ptr)");
 			idx = final_ptr-src;
-			printf("%d,%d\n", idx, idx+target_len);
+			printf("%d,%d\n", idx, idx+target_len-1);
 		}
 		else{
-					puts("HI, in else(final_ptr)");
 			puts("-1");
 		}
 	}
