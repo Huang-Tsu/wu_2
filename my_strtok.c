d6d3e6e3ce5ab6e7889adf3f1f9a02b19a164eaf
Huang-Tsu <76726586+Huang-Tsu@users.noreply.github.com>
2021-03-25 17:05:51 +0800

    Add files via upload

--- my_strtok.c
+++ my_strtok.c
@@ -9,33 +9,62 @@ int main(){
 	char delim[1025];
 	char token[1025];
 	char *ptr;
-	
+
 	while(scanf("%s%s", whole, delim) != EOF){
+		if(strcmp(whole, delim)==0){
+			printf("empty string\n");
+			continue;
+		}
+
 		ptr = whole;
 		ptr = get_tok(ptr, token, delim);
 		printf("%s", token);
-		while( (ptr=get_tok(ptr, token, delim))!=NULL){
-			printf(",%s", token);
+		if(ptr == NULL){
+			puts("");
+			continue;
 		}
-		printf(",%s", token);
-		puts("");
+		else{
+			while( (ptr=get_tok(ptr, token, delim))!=NULL){
+				printf(",%s", token);
+			}
+			puts("");
+		}
 	}
 }
 char *get_tok(char *src, char *token, char *delim){
 	char *ptr = src;
 	char *qtr = token;
 	int delim_len = strlen(delim);
-	int process_len = strlen(src)-delim_len;	
+	int src_len = strlen(src);	
+	char *end = src +src_len;
 
-	for(int i=0; i<=process_len; i++){
+	while(ptr<end && strncmp(ptr, delim, delim_len)==0) ptr+=delim_len;
+	
+	while(ptr<end && strncmp(ptr, delim, delim_len)!=0){
+		*qtr++ = *ptr++;
+	}
+	if(end-ptr<src_len && end-ptr!=0){
+		while(ptr<end){
+			*qtr++ = *ptr;	
+		}
+		return NULL;
+	}
+	else{
+		return ptr;
+	}
+	/*
+	for(int i=0; i<=src_len-delim_len+1; i++){
 		if(strncmp(ptr, delim, delim_len)!=0){
 			*qtr++ = *ptr++;	
 		}
 		else{
+			ptr += delim_len;
+			while(src+src_len-ptr>=delim_len && strncmp(ptr, delim, delim_len)==0){
+				ptr += delim_len;
+			}
 			*qtr = '\0';
-			return ptr+delim_len-1;
+			return ptr;
 		}
 	}
-	*qtr = '\0';
-	return NULL;
+	*/
 }
