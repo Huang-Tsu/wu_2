21240c93a9ccd14459bcd1ef1d0431b471755cc6
Huang-Tsu <huang0966714@gmail.com>
2021-03-30 13:49:10 +0800

    Third edition of postfix Exercise done. completed stack by linked list.

--- postfix.c
+++ postfix.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #define MAX_LINE 4096
-#define MAX_NUMBER 1500
+
 struct node{
 		double number;
 		struct node *next;
@@ -10,7 +10,11 @@ struct node{
 typedef struct node Node;
 
 char *getword(char *input_str, char *word);
-double pop(Node *node);
+double pop(Node **node);
+Node *add_node();
+Node *creat_top_node(double input);
+Node *push(Node *top, double input);
+double operate(double num1, double num2, char *str);
 
 int main(){
 
@@ -20,50 +24,87 @@ int main(){
 	char **end_ptr;		//strtod需要的參數
 	double pop1, pop2, result; 
 	double input_number;
+	Node *top;
 
 
 	while(fgets(line, 4096, stdin) != NULL){
 
+		ptr = line;
 		ptr = getword(ptr, temp_str);	
 		input_number = strtod(temp_str, end_ptr);
-		Node top;
-		top.number = input_number;
-		top.next = NULL;
+		top = creat_top_node(input_number);
 
-		while(ptr=getword(ptr, temp_str) != NULL){
+		while((ptr=getword(ptr, temp_str)) != NULL){
 				if(isdigit(temp_str[0])){
 						input_number = strtod(temp_str, end_ptr);
-						push(top, input_number);
+						top = push(top, input_number);
 				}
 				else{
-					pop1 = pop(top);
-					pop2 = pop(top);
+					pop1 = pop(&top);
+					pop2 = pop(&top);
+
 					result = operate(pop1, pop2, temp_str);
-					push(top, result);
+					top = push(top, result);
 				}
 
 		}
-		result = pop(top);
-		printf("%.2lf\n", result);
+		result = top->number;
+		free(top);
+		printf("%.2f\n", result);
 	}
 
 	return 0;
 }
-char *getword(char *input_str, char *word){
-		char *ptr, *qtr;
-		ptr = input_str;
+char *getword(char *ptr, char *word){
+	
+		char *qtr = word;
 
-		while(*ptr && isspace(ptr)) ptr++;
+		while(*ptr && isspace(*ptr)) ptr++;
 
-		while(*ptr && !isspace(ptr)){
+		while(*ptr && !isspace(*ptr)){
 				*qtr++ = *ptr++;
 		}
+		
+		*qtr = '\0';
 
 		if(word == qtr) return NULL;
 		else return ptr;
 }
-double pop(Node node){
-		if(node.next == NULL){
-				return
-		}
+double pop(Node **top){
+	Node *old_node = *top;
+	double result = (*top)->number;
+	*top = (*top)->next;
+	free(old_node);
+	return result;
+}
+Node *add_node(){
+	return (Node*)calloc(1, sizeof(Node));
+}
+Node *push(Node *top, double input){
+	Node *new_node = add_node();
+	new_node->next = top;
+	new_node->number = input;
+	return new_node;
+}
+double operate(double num1, double num2, char *str){
+	switch(*str){
+		case '+':
+			return num2+num1;	
+			break;
+		case '-':
+			return num2-num1;	
+			break;
+		case '*':
+			return num2*num1;	
+			break;
+		case '/':
+			return num2/num1;	
+			break;
+	}
+}
+Node *creat_top_node(double input){
+	Node *new_node = add_node();
+	new_node->number = input;
+	new_node->next = NULL;
+	return new_node;
 }
