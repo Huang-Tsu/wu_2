5e880083d9301cf4ad24f8863873820dc49ded0f
Huang-Tsu <huang0966714@gmail.com>
2021-04-07 14:50:50 +0800

    Wu exercise - url decode There are few points worth mentioning: 1.After strchr() found %, and copied the string before it into output, now should "++" insted of "+=2". Because the end condition is now == target. 2.In checking whether the character is form A to F, we should use && not||. 3.Declear a variable to record the error message. And before print the final result, we should check it. 4.When switch hex1 and hex2 into character, hex1<<4 shout put () beside→(hex1<<4). 5.Remember to initialize variable.

--- url_decode.c
+++ url_decode.c
@@ -3,54 +3,71 @@
 #include <string.h>
 #include <ctype.h>
 #define MAX_LINE 4096
+#define OK 1
+#define NO 0
 
 int main(){
-
 	char line[MAX_LINE];
-	char *ptr, *qtr, *line_end;
-	char *last;
+	char output[MAX_LINE];
+	char *output_ptr;
+	char *target, *now;
+	char *line_end;
 	int hex1, hex2;
-	unsigned char c;
+	int state;
+	char answer;
 
-	while(fgets(line, MAX_LINE, stdin)){
-		ptr = line;
-		last = line;
-		line_end = line + strlen(line);
-		while(ptr){
-			ptr = strchr(ptr, '%');
-			qtr = last;
-			if(ptr == NULL){
-				while(ptr < line_end){
-					putchar(*qtr);
-					qtr++;
-				}
-				break;
+	while(fgets(line, MAX_LINE, stdin) != NULL){
+		now = line;
+		output_ptr = output;
+		line_end = line+strlen(line);
+		state = OK;
+
+		while((target=strchr(now, '%')) != NULL){
+			if(state == NO) break;
+
+			while(now<target){
+				*output_ptr++ = *now++;
+			}
+			now ++;
+
+			if(isdigit(*now)){
+				hex1 = *now-'0';
+				now++;
+			}
+			else if(*now<='F' && *now>='A'){
+				hex1 = *now-'A'+10;
+				now++;
 			}
 			else{
-				while(qtr < ptr){
-					putchar(*qtr);
-					qtr++;
-				}
-				ptr ++;
-				if(isdigit(*ptr)){
-					hex1 = *ptr - '0';
-				}
-				else{
-					hex1 = *ptr - 'A' + 10;
-				}
-				ptr ++;
-				if(isdigit(*ptr)){
-					hex2 = *ptr - '0';
-				}
-				else{
-					hex2 = *ptr - 'A' + 10;
-				}
-				c = (hex1<<4) | hex2;
-				putchar(c);
-				ptr++;
-				last = ptr;
-				
+				puts("error format");
+				state = NO;
+				break;
 			}
+
+			
+			if(isdigit(*now)){
+				hex2 = *now-'0';
+				now++;
+			}
+			else if(*now<='F' && *now>='A'){
+				hex2 = *now-'A'+10;
+				now++;
+			}
+			else{
+				puts("error format");
+				state = NO;
+				break;
+			}
+			
+			answer = (hex1<<4) + hex2;
+			*output_ptr++ = answer;
+		}
+		while(now<=line_end){
+			*output_ptr++ = *now++;
+		}
+				
+		if(state == OK){
+			printf("%s", output);
 		}
 	}
 }
