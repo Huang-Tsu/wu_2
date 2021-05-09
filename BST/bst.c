2add901eb62cc189d3e30c5b672341e63ce46084
Huang-Tsu <huang0966714@gmail.com>
2021-05-09 15:09:05 +0800

    Starting BST

---
+++ BST/bst.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Tree_node Tree_node 
+
+struct Tree_node{
+	char *word;
+	Tree_node *left;
+	Tree_node *right;
+}
+Tree_node *root;
+
+int main(){
+
+}
