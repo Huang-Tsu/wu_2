734014dec22ee283690cf86086975f49977271c4
Huang-Tsu <huang0966714@gmail.com>
2021-05-10 23:33:00 +0800

    little process

--- BST/bst.c
+++ BST/bst.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Tree_node Tree_node 
+typedef struct Tree_node Tree_node;
 
 struct Tree_node{
 	char *word;
 	Tree_node *left;
 	Tree_node *right;
-}
+};
+
 Tree_node *root;
 
 int main(){
+	root = (Tree_node*)calloc(1, sizeof(Tree_node));
 
 }
