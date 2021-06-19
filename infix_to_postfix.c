287386b4d4c0f5e35995a52dacd464735967b067
Huang-Tsu <huang0966714@gmail.com>
2021-06-19 17:16:31 +0800

    Completed wu_exercise "Infix to Postfix"
    好扯......結尾多了一個空格所以沒過
    也不能說全無收穫
    至少把整個題目想得透徹許多了

--- infix_to_postfix.c
+++ infix_to_postfix.c
@@ -4,51 +4,70 @@
 #include <string.h>
 #define MAX_LINE 3073
 
-void ToPrefix(char *line);
+void ToPostfix(char *line);
 void RmNewLine(char *input);
+
+char output[2*MAX_LINE];
+
 int main(){
 	char line_input[MAX_LINE];
 
 	while(fgets(line_input, MAX_LINE, stdin)){
 		RmNewLine(line_input);
-		ToPrefix(line_input);
+		ToPostfix(line_input);
+
+		printf("%s\n", output);
 	}
 
 	return 0;
 }
-void ToPrefix(char *ptr){
+void ToPostfix(char *ptr){
 	char stack[MAX_LINE];
 	int stack_cnt = 0;
+	char *qtr = output;
+
 	while(*ptr){
 		if(isalnum(*ptr)){
 			while(*ptr && (isalnum(*ptr) || *ptr=='.')){
-				printf("%c", *ptr++);
+				//printf("%c", *ptr++);
+				*qtr++ = *ptr++;
 			}
-			printf(" ");
+			//printf(" ");
+			*qtr++ = ' ';
 		}
 
 		else if(*ptr && (*ptr=='+' || *ptr=='-')){
-			if(stack_cnt==0 || stack[stack_cnt-1]=='(')
-				stack[stack_cnt++] = *ptr++;
+			if(stack_cnt==0 || stack[stack_cnt-1]=='(')		//是stack的第一個 or 前一個是左括
+				;
 			else{
-					printf("%c ", stack[--stack_cnt]);
-				for(int i=stack_cnt; i!=0&&stack[i-1]!='*'&&stack[i-1]!='/'&&stack[i-1]!='('; i--){	
-					printf("%c ", stack[--stack_cnt]);
+				for(int i=stack_cnt; i!=0&&stack[i-1]!='('; i--){	
+					//printf("%c ", stack[--stack_cnt]);
+					*qtr++ = stack[--stack_cnt];
+					*qtr++ = ' ';
 				}
-				stack[stack_cnt++] = *ptr++;
 			}
+
+			stack[stack_cnt++] = *ptr++;
 
 		}
 		else if(*ptr && (*ptr=='*' || *ptr=='/')){
-			if(stack_cnt!=0 && (stack[stack_cnt-1]=='*' || stack[stack_cnt-1]=='/')){
-				printf("%c ", stack[--stack_cnt]);
+			if(stack_cnt==0 || stack[stack_cnt-1]=='+' || stack[stack_cnt-1]=='-' || stack[stack_cnt-1]=='(')
+				;
+			else{
+				for(int i=stack_cnt; i!=0&&stack[i-1]!='+'&&stack[i-1]!='-'&&stack[i-1]!='('; i--){	
+					//printf("%c ", stack[--stack_cnt]);
+					*qtr++ = stack[--stack_cnt];
+					*qtr++ = ' ';
+				}
 			}
 
 			stack[stack_cnt++] = *ptr++;
 		}
 		else if(*ptr && *ptr==')'){
 			for(int i=stack_cnt; stack[i-1]!='('; i--){
-				printf("%c ", stack[--stack_cnt]);
+				//printf("%c ", stack[--stack_cnt]);
+				*qtr++ = stack[--stack_cnt];
+				*qtr++ = ' ';
 			}
 			stack_cnt --;	//扣掉'('
 			ptr ++;
@@ -57,9 +76,14 @@ void ToPrefix(char *ptr){
 			stack[stack_cnt++] = *ptr++;
 	}
 	for(int i=stack_cnt; i!=0; i--){
-		printf("%c ", stack[i-1]);
+		//printf("%c ", stack[i-1]);
+		*qtr++ = stack[--stack_cnt];
+		*qtr++ = ' ';
 	}
-	puts("");
+
+	if(*(qtr-1) == ' ')
+		*(qtr-1) = '\0';
+	*qtr = '\0';
 }
 void RmNewLine(char *input){
 	int len = strlen(input);
